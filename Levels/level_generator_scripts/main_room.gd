b160fecff9e5565b37d04081659e7c8c3e64baf1
marleeeeeey <marleeeeeey@gmail.com>
2024-05-28 01:17:05 +0400

    Enemies randomly moved on the map (lesson 22).

--- Levels/level_generator_scripts/main_room.gd
+++ Levels/level_generator_scripts/main_room.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 @onready var player_scene: PackedScene = preload("res://Entities/Scenes/Player/player.tscn")
 @onready var exit_scene: PackedScene = preload("res://Interactables/scenes/exit.tscn")
+@onready var enemy_scene: PackedScene = preload("res://Entities/Scenes/Enemies/enemy_1.tscn")
 @onready var tilemap = $TileMap
 @export var border = Rect2(1, 1, 140, 120)
 var walker: WalkerRoom = null
-var map: Array = [] # step_history - array of positions. TODO: rename variable
+var map: Array = []  # step_history - array of positions. TODO: rename variable
 
-var ground_layer = 0 # Reference specific png in tilemap. 0 - is the first.
+var ground_layer = 0  # Reference specific png in tilemap. 0 - is the first.
+
 
 func _ready() -> void:
 	randomize()
@@ -20,25 +22,26 @@ func generate_level():
 
 	# List of empty cells.
 	var using_cells: Array = []
-	
-	# Returns a Vector2i array with the positions of all cells containing a tile in the given layer. 
+
+	# Returns a Vector2i array with the positions of all cells containing a tile in the given layer.
 	var all_cells: Array = tilemap.get_used_cells(ground_layer)
-	
+
 	# Clear map and destroy walker.
 	tilemap.clear()
 	walker.queue_free()
-	
+
 	for tile in all_cells:
 		if !map.has(Vector2(tile.x, tile.y)):
 			using_cells.append(tile)
-	
-	# Update all the cells in the cells coordinates array 
+
+	# Update all the cells in the cells coordinates array
 	# so that they use the given terrain for the given terrain_set.
 	tilemap.set_cells_terrain_connect(ground_layer, using_cells, ground_layer, ground_layer, false)
 	tilemap.set_cells_terrain_path(ground_layer, using_cells, ground_layer, ground_layer, false)
-	
+
 	instance_player()
 	instance_exit()
+	instance_enemy()
 
 
 func _input(event):
@@ -57,4 +60,10 @@ func instance_exit():
 	var exit = exit_scene.instantiate()
 	add_child(exit)
 	exit.position = walker.get_end_room().position * 16
-	
+
+
+func instance_enemy():
+	for i in range(32):
+		var enemy = enemy_scene.instantiate()
+		enemy.position = (map.pick_random() * border.position) * 16
+		add_child(enemy)

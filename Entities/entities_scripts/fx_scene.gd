a0f621613a268e866b3dcbfd72108e18fec88300
marleeeeeey <marleeeeeey@gmail.com>
2024-05-28 15:46:03 +0400

    Add sound effects and music.

--- Entities/entities_scripts/fx_scene.gd
+++ Entities/entities_scripts/fx_scene.gd
@@ -4,7 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$anim.play("Active")
-	await  get_tree().create_timer(0.6).timeout
+	$sound.play()
+	await get_tree().create_timer(0.6).timeout
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
